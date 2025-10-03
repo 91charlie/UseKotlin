3f6bf86a3eedb898f2b49d78ba36feaf87668a81
91charlie <cgi1004a@gmail.com>
2025-10-03 11:24:32 +0900

    lexer-wFinal-ing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 package org.example
+import kotlin.reflect.*
 import netscape.javascript.JSObject
 import java.io.File
 import java.lang.StringBuilder
 import java.util.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty0
+import kotlin.reflect.full.memberProperties
 import kotlin.text.iterator
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.primaryConstructor
+
 
 //import kotlinx.serialization.*
 //import kotlinx.serialization.json.*
@@ -13,20 +21,32 @@ import kotlin.text.iterator
 
 //@Serializable
 data class Playlist(
-    val playlistName: String,
-    val createdAt: String,
-    val totalSongs: Int,
-    val totalDuration: Int,
-    val songs: List<Song>
+    var playlistName: String = "",
+    var createdAt: String = "",
+    var totalSongs: Int = 0,
+    var totalDuration: Int = 0,
+    var songs: MutableList<Song> = mutableListOf()
 ){
 }
 
 data class Song(
-    val title: String,
-    val artist: String,
-    val album: String,
-    val duration: Int
+    var title: String = "",
+    var artist: String = "",
+    var album: String = "",
+    var duration: Int = 0
 )
+
+enum class Lexer()
+{
+    key,
+    value,
+    kvseperator,
+    comma,
+    midBoxOpen,
+    midBoxClose,
+    listBoxOpen,
+    listBoxClose
+}
 
 fun String.jsonToker(): List<String>
 {
@@ -45,7 +65,9 @@ fun String.jsonToker(): List<String>
         }
         else if(letter == ':' || letter == ',' || letter == '}')
         {
-            if(letters != "") result.add(letters)
+            if(!letters.isEmpty()){
+                result.add(letters)
+            }
             letters = ""
             result.add(letter.toString())
         }
@@ -66,80 +88,101 @@ fun String.jsonToker(): List<String>
     }
     return result
 }
+fun JsonLexer(tokenedFile: List<String>) : MutableList<Lexer>
+{
+    val fileResult = tokenedFile.reversed()
+    val lexerlist: MutableList<Lexer> = mutableListOf()
+    var read: Boolean = false
+    for(i in fileResult)
+    {
 
+        if(i == "{")
+        {
+            lexerlist.add(Lexer.midBoxOpen)
+        }
+        else if(i == ":")
+        {
+            lexerlist.add(Lexer.kvseperator)
+            read = true
+        }
+        else if(i == "}")
+        {
+            lexerlist.add(Lexer.midBoxClose)
+        }
+        else if(i == "[")
+        {
+            lexerlist.add(Lexer.listBoxOpen)
+        }
+        else if(i == "]")
+        {
+            lexerlist.add(Lexer.listBoxClose)
+        }
+        else if(i == ",")
+        {
+            lexerlist.add(Lexer.comma)
+        }
+        else
+        {
+            if(!read)
+            {
+                lexerlist.add(Lexer.value)
+            }
+            else
+            {
+                lexerlist.add(Lexer.key)
+                read = false
+            }
+        }
+    }
+    return lexerlist.reversed().toMutableList()
+}
 
 
 fun main() {
     val file = File("./src/main/resources/playlist.json") // 현재 디렉토리
 
     val fileResult = file.readText().jsonToker()
+    val lexerlist: MutableList<Lexer> = JsonLexer(fileResult)
+    var playlist = Playlist()
+    var song = Song()
+    val songMirror = song::class.memberProperties.map { it.name }
+    val playlistMirror = playlist::class
 
-    var isOpen: Boolean = false
-    var letters: String = ""
-    val title: MutableList<String> = mutableListOf()
-    val artist: MutableList<String> = mutableListOf()
-    val album: MutableList<String> = mutableListOf()
-    val duration: MutableList<Int> = mutableListOf()
-    var playlistName: String = ""
-    var createdAt: String = ""
-    var totalSongs: Int = 0
-    var totalDuration: Int = 0
+    val a1= Song::title.set(song,fileResult[15])
+    println(songMirror)
+    println(song.title)
 
 
-
-    for(i in 0 until fileResult.size)
+    for(i in 0 until lexerlist.size)
     {
-        if("title" in fileResult[i])
-        {
-            title.add(fileResult[i+2])
-        }
-        else if("artist" in fileResult[i])
-        {
-            artist.add(fileResult[i+2])
-        }
-        else if("album" in fileResult[i])
-        {
-            album.add(fileResult[i+2])
-        }
-        else if("duration" in fileResult[i])
-        {
-            duration.add(fileResult[i+2].toInt())
-        }
-        else if("playlistName" in fileResult[i])
-        {
-            playlistName = fileResult[i + 2]
-        }
-        else if("createdAt" in fileResult[i])
-        {
-            createdAt = fileResult[i + 2]
-        }
-        else if("totalSongs" in fileResult[i])
-        {
-            totalSongs = fileResult[i + 2].toInt()
-        }
-        else if("totalDuration" in fileResult[i])
-        {
-            totalDuration = fileResult[i + 2].toInt()
-        }
-    }
-  for(i in 0 until 5)
-  {
-      println(title[i])
-      println(artist[i])
-      println(album[i])
-      println(duration[i])
-  }
-    println(playlistName)
-    println(createdAt)
-    println(totalSongs)
-    println(totalDuration)
+       var count: Int = 0
+        var SongOrPlayList: Boolean = false
+        var keyName: String = ""
 
-    val instsong = Song(title[0],artist[0],album[0],duration[0])
-    println(instsong)
+       when(lexerlist[i])
+       {
+            Lexer.key -> keyName = fileResult[count]
+            Lexer.value -> TODO()// 분기에 따른 Song or playList :: 키 이름. set(인스턴스, fileResult[count]) value 삽입
+            Lexer.kvseperator -> TODO()
+            Lexer.comma -> TODO()//playlist.songs.add(song)
+            Lexer.midBoxOpen -> TODO()// song 초기화
+            Lexer.midBoxClose -> TODO()
+            Lexer.listBoxOpen -> TODO()// 분기 결정
+            Lexer.listBoxClose ->TODO()// 분기 결정
+       }
+        count++
+   }
+    println("${fileResult.size},${lexerlist.size}")
+
+
 }
+// 리플렉션, 코드 복기, enum 클래스
+// 렉서가 의미와 값을 모두 가지고 있어야한다.
+// 파서의 결과 - AST abstract syntax tree
+// 멤버 프로퍼티로 리스트 만들기 또는 키와 일치하는 이름으로 접근하여 밸류 삽입하기
 
-// 렉서 시도
-// 타이틀, 아티스트, 앨범, 듀래이션을 배열로 만들고 추가한다
-// 플래이리스트네임이나 크레이티드앳 토탈 송스 토탈 듀래이션 은 변수로 만든다
-// 그리고 이것을 song에 모두 합친다
-// song을 songs에 모두 합친다.\
+
+
+// { == 클래스 결정
+// key == 프로퍼티 결정
+// value 클래스.프로퍼티 값 할당
